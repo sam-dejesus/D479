84c999ccb344202070df64f5c6de15ac361f6bd2
samuel de jesus <samuel1de2jesus3@gmail.com>
2024-06-29 12:37:33 -0400

    debug 3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './webpages/Homepage';
 import Lodging from './webpages/Lodging';
 import Sightseeing from './webpages/Sightseeing';
-import Transportation from './webpages/Transportation'
-import Entertainment from './webpages/Entertainment'
+import Transportation from './webpages/Transportation';
+import Entertainment from './webpages/Entertainment';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/Lodging' element={<Lodging /> } />
-        <Route path='Sightseeing' element={< Sightseeing/>} />
-        <Route path='Transportation' element={< Transportation/>} />
-        <Route path='Entertainment' element={< Entertainment/>} />
+        <Route path='/Lodging' element={<Lodging />} />
+        <Route path='/Sightseeing' element={<Sightseeing />} />
+        <Route path='/Transportation' element={<Transportation />} />
+        <Route path='/Entertainment' element={<Entertainment />} />
       </Routes>
     </Router>
   );
