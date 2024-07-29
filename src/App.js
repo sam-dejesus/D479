f1c381ff242f92f49cf753bcd09079afbe6dee45
samuel de jesus <samuel1de2jesus3@gmail.com>
2024-07-29 11:33:14 -0400

    testing update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './webpages/Homepage';
 import Lodging from './webpages/Lodging';
 import Sightseeing from './webpages/Sightseeing';
